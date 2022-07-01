cf9a63e5019d32901084d9ea895a3e149956b95a
RanaAsjal <MC170404422@vu.edu.pk>
2022-07-01 17:36:13 +0500

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿
+namespace ImageUploader
+{
+    partial class frmImageUploader
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnUpload = new System.Windows.Forms.Button();
+            this.txtUpload = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.picImage = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnUpload
+            // 
+            this.btnUpload.Location = new System.Drawing.Point(203, 38);
+            this.btnUpload.Name = "btnUpload";
+            this.btnUpload.Size = new System.Drawing.Size(91, 23);
+            this.btnUpload.TabIndex = 0;
+            this.btnUpload.Text = "Upload";
+            this.btnUpload.UseVisualStyleBackColor = true;
+            this.btnUpload.Click += new System.EventHandler(this.btnUpload_Click);
+            // 
+            // txtUpload
+            // 
+            this.txtUpload.Location = new System.Drawing.Point(74, 12);
+            this.txtUpload.Name = "txtUpload";
+            this.txtUpload.Size = new System.Drawing.Size(355, 20);
+            this.txtUpload.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Select file";
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(434, 10);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 3;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(74, 65);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(355, 10);
+            this.progressBar1.TabIndex = 4;
+            // 
+            // picImage
+            // 
+            this.picImage.Location = new System.Drawing.Point(74, 82);
+            this.picImage.Name = "picImage";
+            this.picImage.Size = new System.Drawing.Size(490, 417);
+            this.picImage.TabIndex = 5;
+            this.picImage.TabStop = false;
+            // 
+            // frmImageUploader
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(576, 511);
+            this.Controls.Add(this.picImage);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtUpload);
+            this.Controls.Add(this.btnUpload);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmImageUploader";
+            this.Text = "Image Uploader";
+            ((System.ComponentModel.ISupportInitialize)(this.picImage)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnUpload;
+        private System.Windows.Forms.TextBox txtUpload;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.PictureBox picImage;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WebAPIFileUpload.Models;
+
+namespace ImageUploader
+{
+    public partial class frmImageUploader : Form
+    {
+        public frmImageUploader()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog opnfd = new OpenFileDialog();
+            opnfd.Filter = "Image Files (*.jpg;*.jpeg;.*.gif;)|*.jpg;*.jpeg;.*.gif";
+            if (opnfd.ShowDialog() == DialogResult.OK)
+            {
+                picImage.Image = new Bitmap(opnfd.FileName);
+                txtUpload.Text = opnfd.FileName;
+            }
+        }
+
+        private void btnUpload_Click(object sender, EventArgs e)
+        {
+            Boolean uploadStatus = false;
+          
+            string url = "http://localhost/WebAPIFileUpload/api/FileHandlingAPI";
+            string filePath = @"\";
+            Random rnd = new Random();
+            string uploadFileName = "Imag" + rnd.Next(9999).ToString();
+            uploadStatus = Upload(url, filePath, txtUpload.Text, uploadFileName);
+
+            if (uploadStatus)
+            {
+                MessageBox.Show("File Uploaded");
+            }
+            else
+            {
+                MessageBox.Show("File Not Uploaded");
+            }
+        }
+
+        bool Upload(string url, string filePath, string localFilename, string uploadFileName)
+        {
+            Boolean isFileUploaded = false;
+
+            try
+            {
+                HttpClient httpClient = new HttpClient();
+             
+                var fileStream = new FileStream(localFilename, FileMode.Open,FileAccess.Read,FileShare.ReadWrite);
+                var fileInfo = new FileInfo(localFilename);
+                UploadFIle uploadResult = null;
+                bool _fileUploaded = false;
+
+                MultipartFormDataContent content = new MultipartFormDataContent();
+                content.Headers.Add("filePath", filePath);
+                content.Add(new StreamContent(fileStream), "\"file\"", string.Format("\"{0}\"", uploadFileName + fileInfo.Extension)
+                        );
+
+                Task taskUpload = httpClient.PostAsync(url, content).ContinueWith(task =>
+                {
+                    if (task.Status == TaskStatus.RanToCompletion)
+                    {
+                        var response = task.Result;
+
+                        if (response.IsSuccessStatusCode)
+                        {
+                            uploadResult = response.Content.ReadAsAsync<UploadFIle>().Result;
+                            if (uploadResult != null)
+                                _fileUploaded = true;
+                        }
+                    }
+                    fileStream.Dispose();
+                });
+
+                taskUpload.Wait();
+                if (_fileUploaded)
+                    isFileUploaded = true;
+                httpClient.Dispose();
+
+            }
+            catch (Exception ex)
+            {
+                isFileUploaded = false;
+            }
+            return isFileUploaded;
+        }
+    }
+}
